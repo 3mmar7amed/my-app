59f2e2c65295f0ef74f798f126c628276f94ca4f
ammar <45490495+3mmar7amed@users.noreply.github.com>
2022-02-03 15:18:51 +0200

    changes

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 const express = require('express');
-const { request } = require('http');
-const { resolve } = require('path');
-const app = express()
-const path = require('path')
+const path = require('path');
+const app = express();
 
+app.use(express.static(path.join(__dirname, 'build')));
 
-const port = process.env.PORT || 50000 ; 
++app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'build')));
-
-    
-    +app.get('/*' , (req ,res) => {
-    
-        req.sendFile(path.join(__dirname, 'build', 'index.html'))
-    })
-    
-}
-
-app.listen(9000 , (err) => {
-
-    if(err) return  console.log(err)
-    console.log("server running on port " , 9000 )
-
-})
+app.listen(9000);
