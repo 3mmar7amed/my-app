59a6f9da7a1e2e907cb0f588c1ec40cf847fafe8
ammar <45490495+3mmar7amed@users.noreply.github.com>
2022-02-03 15:48:26 +0200

    try again to change deploy way

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, 'build')));
 
     
-    +app.get('/' , (req ,res) => {
+    +app.get('*' , (req ,res) => {
     
         req.sendFile(path.join(__dirname, 'build', 'index.html'))
     })
