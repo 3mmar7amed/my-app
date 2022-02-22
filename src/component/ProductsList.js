d010101651db6b2f034365a13180a2e6de521043
ammar <45490495+3mmar7amed@users.noreply.github.com>
2022-02-22 22:12:42 +0200

    change

--- src/component/ProductsList.js
+++ src/component/ProductsList.js
@@ -99,6 +99,7 @@ const ProductsList = ({ Products }) => {
                 </Stack>
                 </div>
                 </div>
+                <hr className="line"  ></hr>
 
 
             </Typography>
